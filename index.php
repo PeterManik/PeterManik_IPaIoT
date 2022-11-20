ed5b24ba09c078787f5e6014271ba6e68986cb84
PeterManik <114649943+PeterManik@users.noreply.github.com>
2022-11-20 16:34:18 +0100

    tets

--- index.php
+++ index.php
@@ -16,21 +16,21 @@
         <input type="text" id="name" name="name" placeholder="Your first name" >
         <input type="text" id="surname" name="surname" placeholder="Your last name" >
         <input type="email" id="email" name="email" placeholder="Your E-mail" >
-        
+        <input type="text" id="postal" name="postal" placeholder="Postal Code">
     
         
     
         <div id="boxes">
         <label>Are you satisfied with the site?</label>
         <label for="Satisfied">Yes</label>
-        <input type="radio" id="Satisfied" name="Satisfied" >
-        <label for="notSatisfied">No</label>
-        <input type="radio" id="notSatisfied" name="Satisfied" >
+        <input type="radio" id="Satisfied" name="Satisfied" value="Yes">
+        
+        <input type="radio" id="notSatisfied" name="Satisfied" value="No">
         </div>
     
         
         <label for="opinion">Please tell us what you think</label>
-        <textarea id="opinion" name="opinion" rows="5" cols="50" ></textarea>
+        <textarea id="opinion" name="opinion" rows="5" cols="50" required></textarea>
     
     
         <input id="submit" type="submit" name="submit" value="Submit" >
@@ -52,14 +52,27 @@ if (isset($_POST['submit'])) {
     $surname=$_POST['surname'];
     $email=$_POST['email'];
     $opinion=$_POST['opinion'];
+    $postal=$_POST['postal'];
 
     $fp = fopen('data.txt', 'a');
+
+    if ($_POST['Satisfied']==Yes) {
+        $satisfied="yes";
+        fwrite($fp, "Satisfied".PHP_EOL);
+    }
+    else {
+        $satisfied="no";
+        fwrite($fp, "Not satisfied".PHP_EOL);
+    }
+
+
     
-    fwrite($fp, $name);
-    fwrite($fp, $surname);
-    fwrite($fp, $email);
-    fwrite($fp, $opinion);
-    echo $name." ".$surname." ".$email." ".$opinion;
+    fwrite($fp, "Name : ".$name.PHP_EOL);
+    fwrite($fp, "Surname :".$surname.PHP_EOL);
+    fwrite($fp, "Email : ".$email.PHP_EOL);
+    fwrite($fp, "Opinion : ".$opinion.PHP_EOL);
+    fwrite($fp, "Postal code : ".$postal.PHP_EOL);
+    echo $name." ".$surname." ".$email." ".$satisfied." ".$opinion." ".$postal;
     fclose($fp);
 }
 
