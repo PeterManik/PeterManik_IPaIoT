7d9956c065599994a29e98dc353c2fa800d5b6d9
PeterManik <114649943+PeterManik@users.noreply.github.com>
2022-12-21 17:13:52 +0100

    vnm

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ filter: drop-shadow(-10px 10px 10px black);
 </div>
 
 <?php
-        echo 'ide';
+        echo 'ide2';
         $file1 = fopen("sensors.txt","r") or die("Unable to open file!");
         $vr = fread($file1, filesize("sensors.txt"));
         $toSplit = explode(";", file_get_contents("sensors.txt"));
