74d22c2fcf68d9543bc0b1fdbafc528df4c11149
PeterManik <114649943+PeterManik@users.noreply.github.com>
2022-12-21 11:20:29 +0100

    inline style

--- index.php
+++ index.php
@@ -2,8 +2,91 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css" >
+    <!--<link rel="stylesheet" href="style.css" >-->
     <title>IoT</title>
+
+
+    <style>
+body{
+
+font-family: Bahnschrift;
+background: url("res/6225108.png") repeat-x;
+animation: slide 60s linear infinite;
+}
+
+#open{
+font-weight: bold;
+background-color: goldenrod;
+border: none;
+border-radius: 7px;
+padding: 2%;
+font-size: 1.3em;
+cursor: pointer;
+filter: drop-shadow(-7px 7px  black);
+}
+
+#open:active{
+filter: drop-shadow(-5px 5px  black);
+transform: translate(-2px, 2px);
+}
+
+
+caption{
+font-size: 5em;
+
+}
+
+.sliding-background{
+display: block;
+height: 100vh;
+width: 100vw;
+
+}
+
+@keyframes slide {
+0%{
+    background-position: 0px 0px;
+}
+100%{
+    background-position: -3000px 0px;
+}
+}
+
+th,td{
+padding-right: 40px;
+font-size: 2em;
+
+}
+
+
+
+button{
+margin: auto;
+}
+
+table{
+margin: auto;
+margin-bottom: 20%;
+margin-top: 10%;
+}
+
+.shadow{
+margin: auto;
+margin-top: 5%;
+display: block;
+max-width: 60%;
+
+padding: 1%;
+padding-left: 2%;
+border-radius: 7px;
+background-color: beige;
+filter: drop-shadow(-10px 10px 10px black);
+}
+
+
+
+
+    </style>
 </head>
 <body>
 
